0e00a7ce6d08627f97fd1babd206419f871374a1
aousalmegrin <aous.almegrin@student.ie.edu>
2022-07-18 22:25:44 +0300

    committing the package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pythonpackagesample',
+setup(name='fairdetect_test',
       version='0.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
-      url='https://github.com/manoelgadi/pythonpackagesample',
+      url='https://github.com/aousalmegrin/fairdetect_test',
       author='Prof. Manoel Gadi',
-      author_email='mfalonso@faculty.ie.edu',
+      author_email='aous.almegrin@student.ie.edu',
       license='MIT',
-          packages=['pythonpackagesample'],
+          packages=['fairdetect_test'],
       zip_safe=False)
